d3d8392b8d9e9f817f020a278f6d0575ecc2373f
Diogo Moretti <diogo.pearljam@gmail.com>
2014-03-04 13:29:12 -0300

    Add canvas and image to canvas for test

--- public/js/main.js
+++ public/js/main.js
@@ -18,15 +18,31 @@ Klipim = {
             
             $.each(json['stickers'], function(index, value) {
                 stickersHTML += '<li class="sticker-item sticker-' + index + '">' +
-                                '<img class="sticker-item-image" src="stickers/' + value[0].path + '" />' +
+                                '<img class="sticker-item-image" id="img-' + index + '" src="stickers/' + value[0].path + '" />' +
                                 '</li>';
             });
 
             s.targetStickersList.html(stickersHTML);
         });
+    },
+
+    addImageToCanvas: function() {
+        canvas.clear(); 
     }
 
 };
+
+var canvas = new fabric.Canvas('c');
+canvas.setWidth(740);
+canvas.setHeight(560);
+
+fabric.Image.fromURL('stickers/gruntjs/gruntjs-preview.svg', function(oImg) {
+    canvas.add(oImg);
+});
+
+$('.sticker-item').on('click', function() {
+    console.log('Click')
+});
 ;(function( window, document, undefined ) {
 
 "use strict";
